6858f869b61b089dc4b6ca85cf5b42b32a50580d
dandy-bookmark-3637 <yamin.gh@gmail.com>
2019-10-25 15:39:03 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,12 +1,8 @@
 def square_array(array)
-  while
+  array = [numbers]
+
+  while array[numbers] do
+    puts array [counter]
+    counter +=1
+  end
 end
-
-# def output_array_elements(array)
-  # counter = 0
-
-  #while array[counter] do
-  #  puts array[counter]
-  #  counter += 1
-  #end
-#end
