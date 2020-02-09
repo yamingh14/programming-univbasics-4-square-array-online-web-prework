b5ac91b9ff20446087787a758d41f5c1513d1358
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-02-09 22:54:54 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,11 @@
-def square_array(array)
-  new_array = []
-  counter = 0
-
-  while counter < array.length do
-    new_array << array[counter] ** 2
-    counter = counter += 1
-  end
-  # return the new array
-  return new_array
-end
+# def square_array(array)
+#   new_array = []
+#   counter = 0
+#
+#   while counter < array.length do
+#     new_array << array[counter] ** 2
+#     counter = counter += 1
+#   end
+#   # return the new array
+#   return new_array
+# end
